7fb366842087e2d491bce63ac1a6b55cd5c8cad7
Tom Willis <tw395@cam.ac.uk>
2025-02-12 01:12:03 +0000

    Fixed up gnomad MAF script to provide EAF column

--- workflow/scripts/gwas/query_lead_snp_maf_with_gnomad.py
+++ workflow/scripts/gwas/query_lead_snp_maf_with_gnomad.py
@@ -28,11 +28,11 @@ query_string = """
                 pos
                 ref
                 alt
-                reference_genome 
+                reference_genome
                 genome {
                 af
                 ac
-                an 
+                an
                 populations {
                     id
                     ac
@@ -42,20 +42,21 @@ query_string = """
                 }
                 }
                 }
-                }
-        """
+            }
+"""
 
 async def query_variant_for_nfe_af(session, rsid):
     query = gql(query_string)
     async with semaphore:
-        await asyncio.sleep(6)  # Space requests to stay within the limit (60 sec / 10 reqs = 6 sec per request)
+        await asyncio.sleep(6)
         try:
-            result = await session.execute(query, variable_values={"rsId": rsid})
+            result = await session.execute(query, variable_values = {"rsId": rsid})
             nfe_dict = [x for x in result['variant']['genome']['populations'] if x['id'] in ['nfe']][0]
             af = nfe_dict['ac']/nfe_dict['an']
-            return pd.DataFrame([{'variant_id': result['variant']['variant_id'], 'rsid': result['variant']['rsid'], 'chrom': result['variant']['chrom'], 'pos': result['variant']['pos'], 'ref': result['variant']['ref'], 'alt': result['variant']['alt'], 'af': af}])
+            return pd.DataFrame([{'rsid': rsid, 'variant_id': result['variant']['variant_id'], 'chrom': result['variant']['chrom'], 'pos': result['variant']['pos'], 'ref': result['variant']['ref'], 'alt': result['variant']['alt'], 'af': af}])
         except TransportQueryError:
-            return nan
+            return pd.DataFrame([{'rsid': rsid, 'variant_id': '', 'chrom': nan, 'pos': nan, 'ref': '', 'alt': '', 'af': nan}])
+
 
 async def main():
     async with client as session:
@@ -65,6 +66,15 @@ async def main():
 
         return results
 
-dafs = asyncio.run(main())
+results = asyncio.run(main())
 
-res_daf = pd.concat(dafs).to_csv(snakemake.output[0], sep = '\t', index = False)
+af_daf = pd.concat(results)
+
+merged = pd.merge(daf, af_daf[['rsid', 'ref', 'alt', 'af']], on = ['rsid'], how = 'left')
+
+merged.loc[(merged['effect_allele'] == merged['alt']) & (merged['other_allele'] == merged['ref']), 'EAF'] = merged['af']
+merged.loc[(merged['effect_allele'] == merged['ref']) & (merged['other_allele'] == merged['alt']), 'EAF'] = 1.0-merged['af']
+
+merged = merged.drop(['ref', 'alt', 'af'], axis = 1)
+
+merged.to_csv(snakemake.output[0], sep = '\t', index = False)
