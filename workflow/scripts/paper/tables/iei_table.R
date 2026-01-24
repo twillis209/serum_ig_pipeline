1e1a6f35d4c53be2258ecfea0b3fed82158caacb
Tom Willis <tom.willis@live.com>
2026-01-24 21:54:53 +0000

    Updated variant columns in IEI table script.

--- workflow/scripts/paper/tables/iei_table.R
+++ workflow/scripts/paper/tables/iei_table.R
@@ -8,13 +8,9 @@ iga[, Isotype := 'IgA']
 igm[, Isotype := 'IgM']
 igg[, Isotype := 'IgG']
 
-iga[, rsID := paste0(rsid, ':', other_allele, '>', effect_allele)]
-igm[, rsID := paste0(rsid, ':', other_allele, '>', effect_allele)]
-igg[, rsID := paste0(rsid, ':', other_allele, '>', effect_allele)]
-
 dat <- rbindlist(lapply(list(iga, igm, igg), function(x) x[, .(Isotype, rsID, chromosome, base_pair_location, Novel, genes, iei_hgnc_symbol, distance_to_iei_gene, IEIs)]))
 
 dat <- dat[!(iei_hgnc_symbol %in% c('IGHM', 'IGKC'))]
 
-fwrite(dat[order(Isotype, chromosome, base_pair_location), .(Isotype, Variant = rsID, Chromosome = chromosome, Position = base_pair_location, Novel, `IEI gene` = iei_hgnc_symbol, `Distance to gene` = distance_to_iei_gene, IEIs)], sep = '\t', file = snakemake@output[[1]])
+fwrite(dat[order(Isotype, chromosome, base_pair_location), .(Isotype, rsID, Chromosome = chromosome, Position = base_pair_location, `Effect allele` = effect_allele, `Other_allele` = other_allele, Novel, `IEI gene` = iei_hgnc_symbol, `Distance to gene` = distance_to_iei_gene, IEIs)], sep = '\t', file = snakemake@output[[1]])
 
